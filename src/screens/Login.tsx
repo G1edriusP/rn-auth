f0313329eaaa204d6c27ff93255ec5ab499b3030
G1edriusP <giedrius.pakalniskis@gmail.com>
2021-11-24 23:51:11 +0200

    Login screen styling

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,22 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCallbackOne } from "use-memo-one";
 
 // Styling
 import styles from "styles/screens/login";
 
 // Components
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Input, Button } from "components";
 import { Logo } from "assets/svg";
 
 // Other
 import { LoginScreenProps } from "constants/types";
 import { NAV } from "constants/navigation";
-import { aBlack, fontMedium, fontRegular, RT } from "constants/brand";
+import { RT } from "constants/brand";
+
+interface UserInfo {
+  email: string;
+  password: string;
+}
+
+enum Fields {
+  email = "email",
+  password = "password",
+}
 
 export default ({ route, navigation }: LoginScreenProps) => {
+  const [user, setUser] = useState<UserInfo>({ email: "", password: "" });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onChangedText = useCallbackOne((field: string, text: string) => {
+    setUser(old => ({ ...old, [field]: text }));
+  }, []);
+
+  const onLoginPress = useCallbackOne(() => {
+    console.log("Login");
+  }, []);
+
+  console.log(user);
+
   return (
     <View style={styles.wrap}>
-      <Logo size={RT(48)} />
-      <Text>Login screen</Text>
+      <View style={styles.content}>
+        <Logo size={RT(48)} />
+        <View style={styles.inputs}>
+          <Input
+            value={user.email}
+            onChangeText={text => onChangedText(Fields.email, text)}
+            editable={!isLoading}
+            placeholder={"Email"}
+          />
+          <Input
+            value={user.password}
+            onChangeText={text => onChangedText(Fields.password, text)}
+            editable={!isLoading}
+            placeholder={"Password"}
+            secureTextEntry
+          />
+        </View>
+      </View>
+      <Button title={"Login"} onPress={onLoginPress} style={styles.button} />
     </View>
   );
 };
--- src/styles/screens/login.ts
+++ src/styles/screens/login.ts
@@ -3,6 +3,10 @@ import { aWhite, sideMargin } from "constants/brand";
 
 interface Styles {
   wrap: ViewStyle;
+  logo: ViewStyle;
+  inputs: ViewStyle;
+  content: ViewStyle;
+  button: ViewStyle;
 }
 
 export default StyleSheet.create<Styles>({
@@ -10,6 +14,25 @@ export default StyleSheet.create<Styles>({
     flex: 1,
     backgroundColor: aWhite,
     alignItems: "center",
+    justifyContent: "space-between",
     padding: sideMargin,
+    paddingTop: sideMargin * 3,
+  },
+  logo: {
+    justifyContent: "center",
+  },
+  content: {
+    flex: 0.6,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  inputs: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  button: {
+    marginTop: sideMargin,
   },
 });
